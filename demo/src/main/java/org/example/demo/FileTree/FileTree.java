9fe794840444ca18640f636f873b3f8170071d48
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-02 08:23:00 +0200

    [DEUXIEME COMMIT] : Ajout de l'application de base : FileTree

---
+++ demo/src/main/java/org/example/demo/FileTree/FileTree.java
@@ -0,0 +1,118 @@
+package org.example.demo.FileTree;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+
+public class FileTree extends Application {
+    HashSet<String> ListeExtension = new HashSet<String>();
+    private TextField champDossier;
+    private TreeView<String> arborescence;
+    private Label cheminComplet;
+    BeginPath chemin = new BeginPath();
+    private final String cheminDeBase = chemin.getCheminDeDebut();;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("FileTree : Rechercheur de fichier");
+        System.out.println(cheminDeBase);
+
+        champDossier = new TextField();
+        champDossier.setPromptText("Entrez le nom du dossier et appuyez sur Entrée");
+
+        cheminComplet = new Label();
+
+        champDossier.setOnAction(event -> {
+            String nomDossier = champDossier.getText();
+            String cheminCompletDossier = cheminDeBase + nomDossier;
+            File repertoire = new File(cheminCompletDossier);
+            if (repertoire.exists() && repertoire.isDirectory()) {
+                TreeItem<String> itemRacine = new TreeItem<>(repertoire.getName());
+                itemRacine.setExpanded(true);
+                ListeExtension.clear();
+                remplirArborescence(repertoire, itemRacine);
+                arborescence.setRoot(itemRacine);
+            } else {
+                champDossier.setText("Nom de dossier invalide");
+            }
+        });
+
+        arborescence = new TreeView<>();
+        arborescence.setOnMouseClicked(event -> {
+            TreeItem<String> elementSelectionne = arborescence.getSelectionModel().getSelectedItem();
+            if (elementSelectionne != null) {
+                StringBuilder constructeurChemin = new StringBuilder();
+                while (elementSelectionne != null) {
+                    constructeurChemin.insert(0, elementSelectionne.getValue());
+                    constructeurChemin.insert(0, File.separator);
+                    elementSelectionne = elementSelectionne.getParent();
+                }
+                cheminComplet.setText("C:" + File.separator + "Users" + File.separator + "Louis" + constructeurChemin.toString());
+            }
+        });
+
+        VBox vbox = new VBox(champDossier, arborescence, cheminComplet);
+
+        Scene scene = new Scene(vbox, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void remplirArborescence(File fichier, TreeItem<String> itemParent) {
+        if (fichier.isDirectory()) {
+            File[] fichiers = fichier.listFiles();
+            if (fichiers != null) {
+                for (File f : fichiers) {
+                    TreeItem<String> item = new TreeItem<>(f.getName());
+                    itemParent.getChildren().add(item);
+                    if (f.isDirectory()){
+                        remplirArborescence(f, item);
+                    } else {
+                        Extension ex = new Extension(item.getValue());
+                        ListeExtension.add(ex.extension);
+                        System.out.println(ListeExtension);
+                        ListeExtension.remove(null);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+class BeginPath extends Exception {
+    private final String cheminDeDebut;
+
+    BeginPath() {
+        Path desktopPath = Paths.get(System.getProperty("user.home"), "Desktop");
+        this.cheminDeDebut = desktopPath.toString() + File.separator;
+    }
+
+    public String getCheminDeDebut() {
+        return cheminDeDebut;
+    }
+}
+
+class Extension extends Exception{
+    private String[] document;
+    public String extension;
+    public Extension(String document){
+        try{
+            if(document.contains(".")) {
+                this.document = document.split("(?=[^.]+$)", 2);
+                extension = "." + this.document[1];
+            }
+        }catch(Exception e){}
+    }
+}
